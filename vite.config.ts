e41a9e0d97873f6c12a91109f681f25abc2194ac
LucasRZamboni <lucasrzamboni@hotmail.com>
2025-03-10 21:36:20 -0300

    changes made

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vehiclePlus/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/vehiclePlus/', // Nome do seu repositório
 });
